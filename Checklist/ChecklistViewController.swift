babaebed6655fa9abfaeace82d6cb992600f1d67
almyk <hansentommy92@gmail.com>
2019-04-01 15:44:45 +0900

    Test making rows in table view

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -16,11 +16,26 @@ class ChecklistViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 100
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        let row = indexPath.row % 5
+        if row == 0 {
+            label.text = "Walk the dog"
+        } else if row == 1 {
+            label.text = "Brush my teeth"
+        } else if row == 2 {
+            label.text = "Learn iOS development"
+        } else if row == 3 {
+            label.text = "Soccer practice"
+        } else if row == 4 {
+            label.text = "Eat ice cream"
+        }
         return cell
     }
 }
